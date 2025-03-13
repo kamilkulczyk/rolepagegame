1d0acc3062a7c7d46ddf23900dc39fa35033e325
kamilkulczyk <kulczykkamil@gmail.com>
2025-03-13 16:20:38 +0100

    fix

--- backend/routes/character_routes.go
+++ backend/routes/character_routes.go
@@ -11,5 +11,5 @@ func CharacterRoutes(app *fiber.App) {
   app.Post("/create-character", middlewares.JWTMiddleware(), handlers.CreateCharacter)
   app.Get("/characters", handlers.GetCharacters)
   app.Get("/characters/:id", handlers.GetCharacterByID)
-  app.Get("/user-characters", middlewares.JWTMiddleware(), handlers.GetCharactersByuserID)
+  app.Get("/user-characters", middlewares.JWTMiddleware(), handlers.GetCharactersByUserID)
 }
