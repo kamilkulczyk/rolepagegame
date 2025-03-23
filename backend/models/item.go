64ad58c8ec4860341e7e98c10640a3821d71a89f
kamilkulczyk <kulczykkamil@gmail.com>
2025-03-23 23:27:29 +0100

    fix

--- backend/models/item.go
+++ backend/models/item.go
@@ -6,13 +6,13 @@ type ItemDetails struct {
   ID      		    int    	 `json:"id"`
   Name	   		    string   `json:"name"`
   Description       string   `json:"description"`
+  UserID 			int      `json:"user_id"`
   ProfileImage      string   `json:"profile_image"`
 }
 
 type ItemRpgData struct {
 	ID          	int                 `json:"id"`
 	ItemID 			int                 `json:"item_id"`
-	UserID 			int                 `json:"user_id"`
 	Type        	string              `json:"type"`
 	Lore        	string              `json:"lore"`
 	Stats       	json.RawMessage		`json:"stats"`
